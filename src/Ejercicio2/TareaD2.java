ed8a2fca109ed2c9cf30c846ed721800ac2b996e
Luis Alejandro <alfaroalejandro597@gmail.com>
2022-10-17 19:36:08 -0600

    clases agregadas

---
+++ src/Ejercicio2/TareaD2.java
@@ -0,0 +1,11 @@
+package Ejercicio2;
+import javax.swing.JOptionPane;
+
+public class TareaD2 {
+    
+   
+    public static void main(String[] args) {
+        LavaPlatos lavaplatosS = new LavaPlatos();
+        lavaplatosS.menuPrincipal();
+    }
+}
